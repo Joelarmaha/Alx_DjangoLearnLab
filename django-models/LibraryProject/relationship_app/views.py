02188f72e3119d97ce43d2df50e3a0a7de6bd8d7
Joel Amah Ayesou <joel.ayesou@gmail.com>
2025-07-18 22:13:39 +0000

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Library
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Function-based view for listing all books
 def list_books(request):
