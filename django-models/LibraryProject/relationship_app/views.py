13998bbbc8279e94b90b717995fcd887a9e8aa80
Joel Amah Ayesou <joel.ayesou@gmail.com>
2025-07-18 22:11:07 +0000

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,9 +9,12 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html", "Book.objects.all()')
 
 # Class-based view to show library details
+"relationship_app/library_detail.html", "from .models import Library"
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+    
 
-    relationship_app/list_books.html", "Book.objects.all(
+    
