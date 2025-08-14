10e8fa10cdfb7c73c2c6195ba1e15bda924fb5b2
Joel Amah Ayesou <joel.ayesou@gmail.com>
2025-08-14 10:11:55 +0000

    Add files via upload

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
-# Create your views here.
+# Registration View
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after registration
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email', request.user.email)
+        request.user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
+
+
